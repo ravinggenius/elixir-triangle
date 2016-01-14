dcbad403e095ad3db22a7324290ee11b48089d76
Thomas Ingram <code@ravinggenius.com>
2016-01-14 18:52:25 -0500

    wip debugging

--- lib/triangle.ex
+++ lib/triangle.ex
@@ -1,3 +1,10 @@
+# require IEx
+
 defmodule Triangle do
-  def area(base, height), do: base * height / 2
+  def area(base, height) do
+    # IO.puts "base: #{base}, height: #{height}"
+    # IO.inspect self
+    # IEx.pry
+    base * height / 2
+  end
 end
